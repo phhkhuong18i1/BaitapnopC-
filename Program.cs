e9d68d80efbceb02ddf5f5525b8b88dd37e4a743
phhkhuong18i1 <48077379+phhkhuong18i1@users.noreply.github.com>
2019-02-28 10:50:41 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("nhap so doan tau:");
+            n = Int32.Parse(Console.ReadLine());
+            doantau[] mangdt = new doantau[100];
+            for (int i=0;i<n;i++)
+            {
+                mangdt[i] = new doantau();
+                mangdt[i].nhap();
+                mangdt[i].intt();
+
+            }
+            doantau tau1 = new doantau();
+            tau1.nhap();
+            tau1.intt();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ tinhtong.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class tinhtong
+    {
+        int i = 1;
+        int n = 0;
+         
+        public void tinh()
+        {
+            Console.WriteLine(" Nhap N =");
+            this.n = Convert.ToInt32(Console.ReadLine());
+        }
+        public double tongla()
+        {
+            int s = 0;
+            for (int i = 1; i <= n; i++) ;
+            if (i % 2 == 0)
+                s =s- i;
+            else
+                s =s+ i;
+            return s;
+        }
+        public void In()
+        {
+            Console.WriteLine(" Tong la {0}", this.tongla());
+        }
+    }
+}
+    
+
